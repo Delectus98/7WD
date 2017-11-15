9d377343a6b7c01d0cdd93490d872a0b4d77fca3
Maxtwist35 <33693052+Maxtwist35@users.noreply.github.com>
2017-11-15 16:53:22 +0100

    Create Textures.java

---
+++ Resources/Textures.java
@@ -0,0 +1,10 @@
+public class Textures{
+    static private HashMap<String, Texture> cards;
+    static private HashMap<String, Texture> utils;
+    static private HashMap<String, Texture> gui;
+    
+    static public void load(){
+      
+    }
+    
+}

757b1c64d0edd04ea831914b1afb2bec98f3cb43
Maxtwist35 <33693052+Maxtwist35@users.noreply.github.com>
2017-11-16 20:09:56 +0100

    Create Files.java

---
+++ Util/Files.java
@@ -0,0 +1,15 @@
+
+
+class Files {
+
+    static public String getFileExtension(String fullPath){}
+    
+    static public String getFileName(String fullPath){}
+    
+    static public String getFilePath(String fullPath){}
+    
+    static public boolean isRegularFile(String fullPath){
+        //return java.util.Files
+    }
+
+}

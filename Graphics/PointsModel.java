8fd4daf226f327fdbae61848ad9ecae5fc432f7b
Maxtwist35 <33693052+Maxtwist35@users.noreply.github.com>
2017-11-16 17:47:55 +0100

    Update PointsModel.java

--- Graphics/PointsModel.java
+++ Graphics/PointsModel.java
@@ -1,34 +1,13 @@
 
 
-public class PointsModel implements gObject{
-    
-    private Sprite laureate = new Sprite();
-    private Text points = new Text();
-    private int count;
-    
+public class PointsModel extends ContentModel{
+        
     public PointsModel(int count){
-        this.count = count;
-        
-        if (this.count < 0)
-          this.count = - this.count;
-        
-        this.points.setString(String.valueOf(this.count));
-        
-        this.points.setOrigin(this.points.getGlobalBounds().width/2, this.points.getGlobalBounds().height/2);
+        super(count);
         
         this.laureate.setTexture(Textures.get("Resources/Textures/laureate.png"), true);
         
         this.laureate.setOrigin(75, 75);
-    }
-    
-    public void setPosition(Vector2f position){
-        this.laureate.setPosition(position);
-        this.points.setPosition(position);
-    }
-    
-    public void draw(RenderTarget target){
-        target.draw(laureate);
-        target.draw(points);
     }
     
 }

86336ace051028ea4daed824d8394c37a4943ab3
Maxtwist35 <33693052+Maxtwist35@users.noreply.github.com>
2017-11-16 20:01:18 +0100

    Update Deck.java

--- Cards/Deck.java
+++ Cards/Deck.java
@@ -4,22 +4,7 @@ public class Deck {
     private String name;
     private ArrayList<ACard> cards;
         
-    
-    public Deck(){}
-    
-    public boolean loadFromFile(String path){
-        //load user deck
-        //  > load existing cards (can't be removed) : "native:name"
-        //  > load user cards "user:{all datas...}"
-        
-        //"native:name" c'est juste pour indiquer que la carte utiliser n'est pas de l'utilisateur et existe déja
-        //"user:{all datas...}" la carte n'existe pas et du coup on a les informations pour la créer
-        //ATTENTION : chaque carte on un nom unique
-        
-        this.name = FileName(path);
-
-    }
-    
+  
     public void shuffle(){ 
         //Mélange de Fisher-Yates
         for (int i=0 ; i < cards.size() ; ++i){
@@ -34,6 +19,7 @@ public class Deck {
     }
     
     
+    //Native Decks
     public static final Deck FirstAgeDeck = createFirstNativeDeck();
     public static final Deck SecondAgeDeck = createSecondNativeDeck();
     public static final Deck ThirdAgeDeck = createThirdNativeDeck();
@@ -42,6 +28,21 @@ public class Deck {
     static private Deck createFirstNativeDeck(){}
     static private Deck createSecondNativeDeck(){}
     static private Deck createThirdNativeDeck(){}
-
+    
+    
+    //User/Player Deck
+    public Deck(){}
+    
+    public boolean loadFromFile(String path){
+        //load user deck
+        //  > load existing cards (can't be removed) : "native:name"
+        //  > load user cards "user:{all datas...}"
+        
+        //"native:name" c'est juste pour indiquer que la carte utiliser n'est pas de l'utilisateur et existe déja
+        //"user:{all datas...}" la carte n'existe pas et du coup on a les informations pour la créer
+        //ATTENTION : chaque carte on un nom unique
+        
+        this.name = FileName(path);
+    }
     
 }

ea93b3ecbdc8d5923754fa9da42e62c40949c70f
Maxtwist35 <33693052+Maxtwist35@users.noreply.github.com>
2017-11-16 22:01:14 +0100

    Update ACard.java

--- Cards/ACard.java
+++ Cards/ACard.java
@@ -4,6 +4,17 @@ import java.util.*;
 public abstract class ACard extends gObject
 {
     private String name;
+    protected Text title;
+    
+    public ACard(String name){
+        this.name = name;
+        
+        this.title.setString(name);
+        
+        this.title.setFont();
+        
+        this.title.setOrigin(this.title.getGlobalBounds().width/2, this.title.getGlobalBounds().height/2);
+    }
     
     public String getName(){
         return name;

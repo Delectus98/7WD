1ea278d21db74fdebcbf90286626a67bcf8fc7cf
Maxtwist35 <33693052+Maxtwist35@users.noreply.github.com>
2017-11-16 19:56:24 +0100

    Update Deck.java

--- Cards/Deck.java
+++ Cards/Deck.java
@@ -4,16 +4,21 @@ public class Deck {
     private String name;
     private ArrayList<ACard> cards;
         
-    public Deck(String name){
-        this.name = name;
-    }
     
     public Deck(){}
     
     public boolean loadFromFile(String path){
-        //create deck/cards from a file
-    }
+        //load user deck
+        //  > load existing cards (can't be removed) : "native:name"
+        //  > load user cards "user:{all datas...}"
+        
+        //"native:name" c'est juste pour indiquer que la carte utiliser n'est pas de l'utilisateur et existe déja
+        //"user:{all datas...}" la carte n'existe pas et du coup on a les informations pour la créer
+        //ATTENTION : chaque carte on un nom unique
+        
+        this.name = FileName(path);
 
+    }
     
     public void shuffle(){ 
         //Mélange de Fisher-Yates
@@ -23,6 +28,7 @@ public class Deck {
             ACard ref = cards[i];
 
             cards[i] = cards[rand];
+            
             cards[rand] = first;
         }       
     }

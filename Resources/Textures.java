c084e9520b539626157d758acdc1c3822c76b55d
Maxtwist35 <33693052+Maxtwist35@users.noreply.github.com>
2017-11-16 17:42:38 +0100

    Update Textures.java

--- Resources/Textures.java
+++ Resources/Textures.java
@@ -1,7 +1,29 @@
 public class Textures{
-    static private HashMap<String, Texture> cards;
+    //miscellaneous
     static private HashMap<String, Texture> utils;
     static private HashMap<String, Texture> gui;
+    
+    //cards
+    static private HashMap<String, Texture> cardsImage;
+
+    static private Texture simpleCardTexture = new Texture();    
+    static private Texture brownTopTexture = new Texture();
+    static private Texture greyTexture = new Texture();
+    static private Texture yellowTopTexture = new Texture();
+    static private Texture blueTopTexture = new Texture();
+    static private Texture greenTopTexture = new Texture();
+    static private Texture redTopTexture = new Texture();
+
+    //models
+    static private Texture goldTexture = new Texture();
+    static private Texture stoneTexture = new Texture();
+    static private Texture woodTexture = new Texture();
+    static private Texture clayTexture = new Texture();
+    static private Texture paperTexture = new Texture();
+    static private Texture glassTexture = new Texture();
+    
+    //structures
+
     
     static public void load(){
       

7abff87509ce62c8996ed6ac4ecac07819a23fcf
Maxtwist35 <33693052+Maxtwist35@users.noreply.github.com>
2017-11-16 18:20:33 +0100

    Update PointsModel.java

--- Graphics/PointsModel.java
+++ Graphics/PointsModel.java
@@ -5,9 +5,9 @@ public class PointsModel extends ContentModel{
     public PointsModel(int count){
         super(count);
         
-        this.laureate.setTexture(Textures.get("Resources/Textures/laureate.png"), true);
+        super.content.setTexture(Textures.get("Resources/Textures/laureate.png"), true);
         
-        this.laureate.setOrigin(75, 75);
+        super.content.setOrigin(75, 75);
     }
     
 }

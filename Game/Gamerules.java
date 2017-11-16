d4754556789ec61de3ab076f84fd26a9e2052062
Maxtwist35 <33693052+Maxtwist35@users.noreply.github.com>
2017-11-16 23:55:04 +0100

    Create Gamerules.java

---
+++ Game/Gamerules.java
@@ -0,0 +1,14 @@
+
+public final class Gamerules {
+    String name;
+    
+    ArrayList<AgePattern> ages; //using Decks 
+    
+    HashMap<String, WonderCard> wonders;
+    HashMap<String, PCoin> coins;
+    
+    public void loadFromFile(String fullpath){
+        //
+    }
+    
+}

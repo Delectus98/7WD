1d81b64398719689001d64c83ca01e710ff39289
Maxtwist35 <33693052+Maxtwist35@users.noreply.github.com>
2017-11-16 16:46:18 +0100

    Update Deck.java

--- Cards/Deck.java
+++ Cards/Deck.java
@@ -1,5 +1,24 @@
 
 
 public class Deck {
-    ArrayList<>
+    private String name;
+    private ArrayList<ACard> cards;
+    
+    public Deck(String name){
+        this.name = name;
+    }
+    
+    public void shuffle(){ 
+        //Mélange de Fisher-Yates
+        for (int i=0 ; i < cards.size() ; ++i){
+            int rand = Math.Random() % cards.size();
+
+            ACard ref = cards[i];
+
+            cards[i] = cards[rand];
+            cards[rand] = first;
+        }       
+    }
+    
+    
 }

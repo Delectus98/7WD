9615ceb1dca421097caddb8a0793d0dad0b79e67
Maxtwist35 <33693052+Maxtwist35@users.noreply.github.com>
2017-11-16 22:36:24 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+
+public class Main {
+    public static void main(String[] args){
+        Textures.load();
+        Fonts.load();
+        Sounds.load();
+        Musics.load();
+        
+        
+    }
+}

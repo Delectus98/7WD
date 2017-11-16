ec769ae0d4bd629aafea2ecd8cec5ff5bbdc477a
Maxtwist35 <33693052+Maxtwist35@users.noreply.github.com>
2017-11-16 16:36:16 +0100

    Create Cards.java

---
+++ Cards/Cards.java
@@ -0,0 +1,32 @@
+
+public class Cards {
+    
+    public static isBrownCard(ACard card){
+        return (card instanceof BrownCard);
+    }
+    
+    public static isGreyCard(ACard card){
+        return (card instanceof GreyCard);
+    }
+
+    public static isYellowCard(ACard card){
+        return (card instanceof YellowCard);
+    }
+
+    public static isRedCard(ACard card){
+        return (card instanceof RedCard);
+    }
+
+    public static isGreenCard(ACard card){
+        return (card instanceof GreenCard);
+    }
+    
+    public static isBlueCard(ACard card){
+        return (card instanceof BlueCard);
+    }
+    
+    public static isWonderCard(ACard card){
+        return (card instanceof WonderCard);
+    }
+    
+}

a07dee5275ce743d85e68843afe4b6d3bc3e63d1
Maxtwist35 <33693052+Maxtwist35@users.noreply.github.com>
2017-11-16 17:44:02 +0100

    Update MaterialsModel.java

--- Graphics/MaterialsModel.java
+++ Graphics/MaterialsModel.java
@@ -1,11 +1,11 @@
 
 
-public class MaterialsModel implements gObject{
+public abstract class MaterialsModel implements gObject{
     private Sprite sprite = new Sprite();
     private Text amouth = new Title();
     private int count;
     
-    public MaterialsModel(int count, ResType type){
+    public MaterialsModel(int count){
         this.count = count;
         
         if (this.count < 0)

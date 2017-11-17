14a26e990e48ce283d46e5b6ff090c863d9b6e89
Delectus98 <33693052+Delectus98@users.noreply.github.com>
2017-11-17 22:39:40 +0100

    Update and rename Util/InternalScript.java to Game/GameScripts.java

---
+++ Game/GameScripts.java
@@ -0,0 +1,40 @@
+
+
+public class GameScripts {
+    private ArrayList<String> lines;
+    private Trigger trigger = null;
+    //private ArrayList<> compiled
+    
+    // Memory cache
+    private <String> acardvar;
+    private <String> wonderscards;
+    private <String> progressTokens;
+    private <String[]> strsvars;
+    private <int> ivar;
+    
+    
+    
+    public InternalScript(String[] data, Trigger trigger){
+        
+        this.trigger = trigger;
+    }
+    
+    public boolean loadFromMemory(String[] lines){
+    
+    }
+    
+    public void onTrigger(Trigger trigger){
+        //trigger est une fonction, une action qui teste pour chaque script disponible si qqc doit agir
+        if (this.trigger.sameAs(trigger))){
+            this.execute();
+        }
+    } 
+    
+    public void execute(){
+        
+    }
+    
+    public loadFromMemory(String[] args){
+        
+    }
+}

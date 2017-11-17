499cf93ab322d2b8ff19649de96f2092079c4956
Maxtwist35 <33693052+Maxtwist35@users.noreply.github.com>
2017-11-17 13:07:50 +0100

    Update Deck.java

--- Cards/Deck.java
+++ Cards/Deck.java
@@ -3,8 +3,31 @@
 public final class Deck {
     //User decks
     static private HashMap<String, ACard> userDecks;
-    //si un deck existe déja alors on en rajoute pas une et on renvoie false
-    //les deck utilisateurs sont limités (pas plus de XXX >> à définir) et ils sont générés que si ils sont demandés
+        //si un deck existe déja alors on en rajoute pas une et on renvoie false
+        //les deck utilisateurs sont limités (pas plus de XXX >> à définir) et ils sont générés que si ils sont demandés
+    public Deck(){}
+    
+    public boolean loadFromFile(String fullpath){
+        //load user deck
+        // deck name est unique
+        //  > load existing cards (can't be removed) : "native:name"
+        //  > load user cards "user:{all datas...}"
+        
+        //"native:name" c'est juste pour indiquer que la carte utiliser n'est pas de l'utilisateur et existe déja
+        //"user:{all datas...}" la carte n'existe pas et du coup on a les informations pour la créer
+        //ATTENTION : chaque carte on un nom unique
+        
+        if (Files.getFileExtension(fullpath).equals("deck")){
+            //on doit trouver la bonne extension du fichier 
+            
+            this.name = Files.getFileName(fullpath);
+
+            return true;
+        } else {
+            return false;   
+        }
+        
+    }
     
     static public boolean addDeck(Deck deck){}
     
@@ -46,30 +69,13 @@ public final class Deck {
         }       
     }
     
+    public int size(){
+        return cards.size();
+    }
+    
+    public ACard get(int i){}
     
     //User/Player Deck
-    public Deck(){}
     
-    public boolean loadFromFile(String fullpath){
-        //load user deck
-        // deck name est unique
-        //  > load existing cards (can't be removed) : "native:name"
-        //  > load user cards "user:{all datas...}"
-        
-        //"native:name" c'est juste pour indiquer que la carte utiliser n'est pas de l'utilisateur et existe déja
-        //"user:{all datas...}" la carte n'existe pas et du coup on a les informations pour la créer
-        //ATTENTION : chaque carte on un nom unique
-        
-        if (Files.getFileExtension(fullpath).equals("deck")){
-            //on doit trouver la bonne extension du fichier 
-            
-            this.name = Files.getFileName(fullpath);
-
-            return true;
-        } else {
-            return false;   
-        }
-        
-    }
     
 }

0cebd12ea483cc3e5332802565547393bcd34746
Delectus98 <33693052+Delectus98@users.noreply.github.com>
2017-11-17 17:12:27 +0100

    Update ChainSymbols.java

--- Cards/ChainSymbols.java
+++ Cards/ChainSymbols.java
@@ -1,8 +1,26 @@
 
 
 public class ChainSymbols {
-    private HashMao<String, Symbol> symbols;
+    public static class Symbol{
+        private String name;
+        private Texture texture = null;
+        
+        private Symbol(String name, Texture texture){
+            this.name = new String(name);
+            this.texture = texture;
+        }
+    }
     
+    private static HashMap<String, Symbol> symbols;
+    
+    public static boolean addSymbol(String name, String textureName){
+        if (symbols.find(name)){
+            return false;
+        } else {
+            symbols.add(name, new Symbol(name, Texture.get(TextureName)));
+            return true;   
+        }
+    }
     //priceSymbol
     //chainSymbol
 }

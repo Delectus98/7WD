76ad60774c3c79ee813134632dc89bc3d6f922d5
maxjbh <33730588+maxjbh@users.noreply.github.com>
2017-11-17 21:47:17 +0100

    Update Sounds.java

--- Resources/Sounds.java
+++ Resources/Sounds.java
@@ -3,7 +3,9 @@
 public class Sounds {
     private HashMap<String, SoundBuffer> buffers;
     
-    public boolean preload(){}
+    public boolean preload(){
+        return true;
+    }
     
     public SoundBuffer get(String name){
         return buffers.get(name);

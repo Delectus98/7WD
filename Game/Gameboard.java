84cfd7c4c62372a08c1ba9f7a51761f4ae86c2f9
Delectus98 <33693052+Delectus98@users.noreply.github.com>
2017-11-17 13:42:18 +0100

    Create Gameboard.java

---
+++ Game/Gameboard.java
@@ -0,0 +1,12 @@
+
+
+public class Gameboard {
+    private ProgressToken[] ptokens = new ProgressToken[X];
+    private MilitaryToken[] mtokens = new MilitaryToken[Y];
+    
+    private Sprite militaryPawn;
+    
+    public Gameboard(Gamerules rules){}
+       
+}
+

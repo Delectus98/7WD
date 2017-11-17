53bf41f1585ce053e3669fbde6b9cbf56b06f2de
maxjbh <33730588+maxjbh@users.noreply.github.com>
2017-11-17 21:52:14 +0100

    Update Fonts.java

--- Resources/Fonts.java
+++ Resources/Fonts.java
@@ -3,7 +3,9 @@
 public class Fonts {
     private HashMap<String, Font> fontList;
     
-    public static void preload(){}
+    public static boolean preload(){
+        return true;
+    }
     
     public static Font get(String name){
         return fontList.get(name);

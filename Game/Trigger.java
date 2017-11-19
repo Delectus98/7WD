03253ddaa90609f3158fe65eee6cca34623bc9bd
Delectus98 <33693052+Delectus98@users.noreply.github.com>
2017-11-19 23:15:32 +0100

    Update Trigger.java

--- Game/Trigger.java
+++ Game/Trigger.java
@@ -3,8 +3,8 @@
 public class Trigger {
     public void find(){
         for (playerA in wonders)
-        
+            wonder.hasSame(this);
         for (playerB in wonders)
-        
+            wonder.hasSame(this);
     }
 }

571832b9fe46538ef5c730e2a03c51c25c14210a
Delectus98 <33693052+Delectus98@users.noreply.github.com>
2017-11-20 11:50:42 +0100

    Update WonderCard.java

--- Cards/WonderCard.java
+++ Cards/WonderCard.java
@@ -2,4 +2,12 @@
 
 public class WonderCard extends ACard{
     private GameScript internalScript;
+    private Trigger trigger;
+    
+    public void listen(Trigger trigger){
+        if (this.trigger == trigger){ 
+            //ou autre comparaison
+            internalScript.execute();
+        }
+    }
 }

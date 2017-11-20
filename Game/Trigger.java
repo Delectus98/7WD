fa5c51b437f51ee3f233078fc70fe21a116341de
Delectus98 <33693052+Delectus98@users.noreply.github.com>
2017-11-20 12:00:27 +0100

    Update Trigger.java

--- Game/Trigger.java
+++ Game/Trigger.java
@@ -1,10 +1,30 @@
 
 
+
+
 public class Trigger {
-    public void find(){
-        for (playerA in wonders)
-            wonder.hasSame(this);
-        for (playerB in wonders)
-            wonder.hasSame(this);
-    }
+    private String whatHappened;
+    
+    @Override //none copyable
+    private Trigger copy(){ return; }
+    
+    private Trigger(String whatHapenned){}
+    
+    private Trigger(String whatHapenned, String withWhat, int count){}
+
+    
+    static public void create(String whatHappened){}
+    
+    static public void create(String whatHappened, String withWhat, int count){}
+
+    static public Trigger get(String whatHappened){}
+    
+    //all possible events && all dynamics events (dynamics == with different way to do a same)
+    static private Trigger onBuyingResources = new Trigger("onBuyingResources");
+    static private Trigger onBuyingResource = new Trigger("onBuyingResource", "Stone", 1);
+    static private Trigger onDeleteCard = new Trigger("onDeleteCard");
+    static private Trigger onBuildCard = new Trigger("onBuildCard");
+    static private Trigger onBuyingCard = new Trigger("onBuyingCard");
+
 }
+

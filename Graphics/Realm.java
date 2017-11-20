33a75cd6404abcceea07775e8ae561063c3eb5d4
Delectus98 <33693052+Delectus98@users.noreply.github.com>
2017-11-20 20:38:35 +0100

    Update Realm.java

--- Graphics/Realm.java
+++ Graphics/Realm.java
@@ -3,4 +3,8 @@
 public class Realm extends gObject{
     private ArrayList<IsometricModel> models;
     private Army[2] armies;
+    
+    public void add(IsometricModel model){
+        //find a place
+    }
 }

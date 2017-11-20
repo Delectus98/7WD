ba856f66b933e20eae51422df27ac649a0851f63
Delectus98 <33693052+Delectus98@users.noreply.github.com>
2017-11-20 16:56:40 +0100

    Update BrownCard.java

--- Cards/BrownCard.java
+++ Cards/BrownCard.java
@@ -1,8 +1,12 @@
 
 public class BrownCard extends ACard{
+    //values
     private int wood = 0;
     private int stone = 0;
     private int clay = 0;
+    
+    //graphics
+    private ArrayList<ResourcesModel> models;
     
     public BrownCard(String name, int wood, int stone, int clay){
         super(name);
@@ -10,5 +14,18 @@ public class BrownCard extends ACard{
         this.wood = wood;
         this.stone = stone;
         this.clay = clay;
+        
+        if (wood != 0){
+            WoodModel temp = new WoodModel();
+
+        }
+        
+        if (stone != 0){
+            StoneModel temp = new StoneModel();
+        }
+        
+        if (clay != 0){
+            ClayModel temp = new ClayModel();
+        }
     }
 }

89178d3a9166d0a5f2f5f1a40313570d747730d9
Delectus98 <33693052+Delectus98@users.noreply.github.com>
2017-11-20 17:13:37 +0100

    Update GreyCard.java

--- Cards/GreyCard.java
+++ Cards/GreyCard.java
@@ -1,6 +1,35 @@
 
 
 public class GreyCard extends ACard{
+    //values   
     private int glass = 0;
     private int paper = 0;
+    
+    //graphics
+    private Sprite banner = new Sprite();
+    private ArrayList<MaterialModel> models;
+    
+    public GreyCard(String name, int glass, int paper){
+        this.glass = glass;
+        this.paper = paper;
+    
+        if (this.glass != 0){
+            models.add(new GlassModel(this.glass));
+            
+        }
+        
+        if (this.paper != 0){
+            models.add(new PaperModel(this.paper));
+
+        }
+    }
+    
+    @Override
+    public void draw(RenderTarget target){
+        target.draw(banner);
+        
+        for (MaterialModel model : models){
+            models.draw(target);
+        }
+    }
 }

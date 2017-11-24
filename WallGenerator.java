066c4824072cf2fd2b8fd15a8254ff17a79e427c
Delectus98 <33693052+Delectus98@users.noreply.github.com>
2017-11-24 13:39:51 +0100

    Create WallGenerator.java

---
+++ WallGenerator.java
@@ -0,0 +1,14 @@
+
+
+public class WallGenerator extends Wall{
+    private Monster monsterType = null;
+    
+    pubic WallGenerator(World world, Monster type){
+        super (world, Assets.....);
+        this.monsterType = type;
+    }
+    
+    //trouver un moyen d'updater certains types de Structures
+    public void update(....){}
+    
+}

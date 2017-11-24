4c534410e433b56f29433b3d11c4cc9afb5bfb1b
Delectus98 <33693052+Delectus98@users.noreply.github.com>
2017-11-24 13:42:01 +0100

    Update WallGenerator.java

--- WallGenerator.java
+++ WallGenerator.java
@@ -1,10 +1,14 @@
 
 
 public class WallGenerator extends Wall{
+    //Génère des monstres aux alentours de la tuile ou est positionné le mur
+    
+    private int radius = 0;
     private Monster monsterType = null;
     
-    pubic WallGenerator(World world, Monster type){
+    pubic WallGenerator(World world, int radius, Monster type){
         super (world, Assets.....);
+        this.radius = radius;
         this.monsterType = type;
     }
     

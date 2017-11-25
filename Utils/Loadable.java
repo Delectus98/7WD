281ca6b3604a2511472960599ca229b4bbe471ff
Delectus98 <33693052+Delectus98@users.noreply.github.com>
2017-11-25 19:48:27 +0100

    Update Loadable.java

--- Utils/Loadable.java
+++ Utils/Loadable.java
@@ -1,5 +1,17 @@
 
 
-public interface Loadable {
-    public void loadFromFile(String namespace);
+public abstract class Loadable {
+    private String namespace;
+    private String name;
+    
+    public boolean loadFromFile(String namespace, String name);
+    
+    public String getNamespace(){
+        return namespace;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
 }

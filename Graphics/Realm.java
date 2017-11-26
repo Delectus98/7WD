04c5ea87f54d7c285d0daf0b1d55d5b93bf716aa
Delectus98 <33693052+Delectus98@users.noreply.github.com>
2017-11-26 15:24:21 +0100

    Update Realm.java

--- Graphics/Realm.java
+++ Graphics/Realm.java
@@ -3,6 +3,7 @@
 public class Realm extends gObject{
     
     private ArrayList<IsometricModel> models = new ArrayList<>();
+    private IsometricModel[][] city;
     
     public void add(IsometricModel model){
         //find a place

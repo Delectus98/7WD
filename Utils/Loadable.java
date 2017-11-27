b95c33a4913b34dca19f3eed055c348a8926e6c4
Delectus98 <33693052+Delectus98@users.noreply.github.com>
2017-11-27 20:21:31 +0100

    Update Loadable.java

--- Utils/Loadable.java
+++ Utils/Loadable.java
@@ -4,7 +4,7 @@ public abstract class Loadable {
     private String namespace;
     private String name;
     
-    public boolean loadFromFile(String namespace, String name);
+    public abstract boolean loadFromFile(String namespace, String name);
     
     public String getNamespace(){
         return namespace;

ad90da33136bb622086018d993bf4fd3348683a4
Delectus98 <33693052+Delectus98@users.noreply.github.com>
2017-11-27 20:06:08 +0100

    Update Loop.java

--- Utils/ScriptKeywords/Loop.java
+++ Utils/ScriptKeywords/Loop.java
@@ -2,18 +2,9 @@
 
 public abstract class Loop extends CodeFragment {
     protected ArrayList<CodeFragment> inside;
-    protected Condition condition;
     
     public Loop(){}
-    
-    public Loop(Condition condition){
-        this.condition = condition;
-    }
-    
-    public void setCondition(Condition condition){
-        this.condition = condition;
-    }
-    
+   
     public void addCodeFragment(CodeFragment frag){
         frag.add(frag);
     }

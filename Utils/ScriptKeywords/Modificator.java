36d7e082b257ed39919870e40010e5ed1d59829e
Delectus98 <33693052+Delectus98@users.noreply.github.com>
2017-11-28 21:50:41 +0100

    Update Modificator.java

--- Utils/ScriptKeywords/Modificator.java
+++ Utils/ScriptKeywords/Modificator.java
@@ -1,12 +1,43 @@
 
 
-public abstract class Modificator extends CodeFragment {
+public abstract class Modificators {
+    //params
+    Player currentPlayer;
+    Player opponentPlayer;
     
     @Override
-    public void execute(){}
-    
-    @Override
-    public void modify(){}
+    public static String execute(String function, String args[]){
+        //Player|Opponent functions
+        if (function.equals("Player.earnMoney")){ if (args.length == 1){
+                int value;
+                try { value = Interger.parseInt(execute(args[0])); } catch () { value = 0; } finnaly { currentPlayer.earnMoney(value); }
+            } else {
+                return "<initialise type=\"exception\" name=\"exception\" constructor=\"Player.earnMoney\">";
+            }
+        } 
+        else if (function.equals("Opponent.earnMoney")){ if (args.length == 1){
+                int value;
+                try { value = Interger.parseInt(execute(args[0])); } catch () { value = 0; } finnaly { opponentPlayer.earnMoney(value); }
+            } else {
+                return "<initialise type=\"exception\" name=\"exception\" constructor=\"Opponent.earnMoney\">";
+            }
+        }
+        else if (function.equals("Player.lossMoney")){ if (args.length == 1){
+                int value;
+                try { value = Interger.parseInt(execute(args[0])); } catch () { value = 0; } finnaly { currentPlayer.lossMoney(value); }
+            } else {
+                return "<initialise type=\"exception\" name=\"exception\" constructor=\"Player.lossMoney\">";
+            }
+        }
+        else if (function.equals("Opponent.lossMoney")){ if (args.length == 1){
+                int value;
+                try { value = Interger.parseInt(execute(args[0])); } catch () { value = 0; } finnaly { opponentPlayer.lossMoney(value); }
+            } else {
+                return "<initialise type=\"exception\" name=\"exception\" constructor=\"Opponent.lossMoney\">";
+            }
+        }
+        //Gameboard functions
+    }
     
     
 }

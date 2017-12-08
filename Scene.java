45f4f1a8a2d92b6945c091613df95a0683a39c86
Delectus98 <33693052+Delectus98@users.noreply.github.com>
2017-12-08 11:19:17 +0100

    Update Scene.java

--- Scene.java
+++ Scene.java
@@ -8,3 +8,19 @@ public Scene extends Screen {
      
     public void update(){}
 }
+
+
+String[] vertex = 
+{
+    "uniform vec2 usingCanvasPosition;",
+    "",
+    "void main()",
+    "{",
+    "   gl_Position = gl_ModelViewProjectionMatrix * usingCanvasPosition.xy;",
+    "   gl_TexCoord[0] = gl_TextureMatrix[0] * gl_MultiTexCoord0;"
+    "   gl_FrontColor = gl_Color;"
+    "}"
+};
+
+this.shader.setParameter("usingCanvasPosition", canvases.get(i).localPos);
+target.draw(canvases.get(i).content, this.shader);

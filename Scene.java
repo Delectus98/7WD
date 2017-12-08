d164ab0c0150f9f60abbef1c177f814516aa89ce
Delectus98 <33693052+Delectus98@users.noreply.github.com>
2017-12-08 11:01:51 +0100

    Update Scene.java

--- Scene.java
+++ Scene.java
@@ -1,6 +1,9 @@
 public Scene extends Screen {
+    private final SceneControler controler;
      
-    public void add(ButtonState ){}
+    public void add(Button button, Screen nextScreen){
+          button.setEvent(Button.RELEASED, () -> {controler.setCurrentScreen(nextScreen)});
+    }
      
     public void update(){}
 }

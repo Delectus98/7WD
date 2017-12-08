21d0bafafcd86f14c044da30c5d70d81fad11b3b
Delectus98 <33693052+Delectus98@users.noreply.github.com>
2017-12-08 11:55:36 +0100

    Create WonderCard.java

---
+++ WonderCard.java
@@ -0,0 +1,24 @@
+public interface Cloneable<T> {
+    T clone();
+}
+
+
+public class WonderCard implements Cloneable<WonderCard>{
+    private Action action;
+    
+    private static final HashMap<String, WonderCard> nativeWonders;
+    public static natives(){
+        nativeWonders.
+    }
+    public static WonderCard get(String name){
+        if (nativeWonders.get(name))
+    }
+    
+    public WonderCard(String name, Action action){
+        
+    }
+    
+    public WonderCard clone(){
+        
+    }
+}

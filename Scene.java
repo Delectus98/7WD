3cbcb6a4001535ecfa9715682707006789c65e40
Delectus98 <33693052+Delectus98@users.noreply.github.com>
2017-12-08 11:09:48 +0100

    Update Scene.java

--- Scene.java
+++ Scene.java
@@ -1,9 +1,10 @@
-public Scene extends Screen {
+/**
+    On pourrait utiliser plusieurs fois le même bouton et il serais positionné selon le canvas qui veut le dessiner.
+*/
     private final SceneControler controler;
-     
-    public void add(Button button, Screen nextScreen){
-          button.setEvent(Button.RELEASED, () -> {controler.setCurrentScreen(nextScreen)});
-    }
+    @FunctionalInterface CanvasAction/GenericAction;
+
+public Scene extends Screen {
      
     public void update(){}
 }
